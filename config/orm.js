f2a53575b0045b006e9900b18faf08329a7077ce
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-10 00:26:23 -0600

    orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,19 @@
 require('./connection.js');
 
-query.connection("SELECT * FROM burgers", function(err, res){
-	if(err) throw err;
-});
+var queries = function (burger){
 
-query.connection("INSERT INTO burgers WHERE ", function(err, res){
-	if(err) throw err;
-});
+	query.connection("SELECT * FROM burgers", [],function(err, res){
+		if(err) throw err;
+	});
 
-query.connection("UPDATE burgers WHERE", function(err, res){
-	if(err) throw err;
-});
+	query.connection("INSERT INTO burgers WHERE ?", [],function(err, res){
+		if(err) throw err;
+	});
+
+	query.connection("UPDATE burgers WHERE ?", [],function(err, res){
+		if(err) throw err;
+	});
+
+}
+
+module.exports = queries;
